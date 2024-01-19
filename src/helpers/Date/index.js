98cb0766e7740d687836e21c4a4a7d998561c266
Yoan Gvs <yoangovaerts@gmail.com>
2024-01-19 05:29:14 +0100

    IW-85 corretion indexation des mois

--- src/helpers/Date/index.js
+++ src/helpers/Date/index.js
@@ -13,4 +13,5 @@ export const MONTHS = {
   12: "décembre",
 };
 
-export const getMonth = (date) => MONTHS[date.getMonth()];
+export const getMonth = (date) => MONTHS[date.getMonth()+1];
+// pour que janvier ne soit plus a 0 mais a 1
